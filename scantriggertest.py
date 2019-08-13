16bd2414abeeecd9d19db4d2dad788d29c64d65c
kellyhennigan <kelhennigan@gmail.com>
2019-08-13 15:05:45 -0700

    edits

--- scantriggertest.py
+++ scantriggertest.py
@@ -1,3 +1,7 @@
+import sys, signal, time, os
+from datetime import datetime
 from utilities import start_scanner
 
-start_scanner()
+pulsetime = start_scanner()
+cmd = 'echo scan trigger time: '+str(pulsetime)+' >> afile'
+os.system(cmd)
